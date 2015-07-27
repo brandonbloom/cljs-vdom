b736224e5b1da44e7681db51469b0fa05f403c95
Brandon Bloom <brandon@brandonbloom.name>
2015-07-26 19:58:03 -0700

    set version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject bbloom.vdom "0.1.0-SNAPSHOT"
+(defproject bbloom.vdom "0.0.1"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
