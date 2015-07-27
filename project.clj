eff562080e2c86b052fe7c06ea0bc633f117ade3
Brandon Bloom <brandon@brandonbloom.name>
2015-07-26 19:58:59 -0700

    Fill in project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject bbloom.vdom "0.0.1"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A ClojureScript virtual-dom library."
+  :url "https://github.com/brandonbloom/cljs-vdom"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
